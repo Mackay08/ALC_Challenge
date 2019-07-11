be8c47a59c2a8cd46ba9574eba1442f3898b6894
Mackay08 <mackay.sappor@gmail.com>
2019-07-11 12:41:36 +0000

    First Commit

---
+++ app/src/main/java/com/antoniolabs/ai/alcchallenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.antoniolabs.ai.alcchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
